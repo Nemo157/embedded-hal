5c2a5ca096627a245a962e017e70d73369c337bf
Wim Looman <wim.looman@kiwi.ki>
2018-03-07 17:17:39 +0100

    Implement nb::io::Read trait for proposed rng trait

--- src/rng.rs
+++ src/rng.rs
@@ -15,3 +15,47 @@ pub trait Read {
     /// Get a single byte from the RNG
     fn read(&mut self) -> nb::Result<u8, Self::Error>;
 }
+
+#[cfg(feature = "unproven")]
+/// TODO
+pub mod io {
+    use nb;
+    use super::Read;
+
+    /// TODO
+    pub struct Reader<R> where R: Read {
+        reader: R,
+    }
+
+    /// TODO
+    pub fn reader<R>(reader: R) -> Reader<R> where R: Read {
+        Reader { reader }
+    }
+
+    impl<R> nb::io::Read for Reader<R> where R: Read {
+        type Error = R::Error;
+
+        fn read(&mut self, buf: &mut [u8]) -> nb::Result<usize, Self::Error> {
+            let mut count = 0;
+            while count < buf.len() {
+                match self.reader.read() {
+                    Ok(byte) => {
+                        buf[count] = byte;
+                        count += 1;
+                    }
+                    Err(nb::Error::WouldBlock) => {
+                        if count > 0 {
+                            return Ok(count);
+                        } else {
+                            return Err(nb::Error::WouldBlock);
+                        }
+                    }
+                    Err(error) => {
+                        return Err(error);
+                    }
+                }
+            }
+            return Ok(count);
+        }
+    }
+}
