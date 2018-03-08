2d7ec95b5bcf259ccc62140ae292e13347a661bf
Wim Looman <wim.looman@kiwi.ki>
2018-03-08 14:27:10 +0100

    Add API for detecting events on an input pin

--- src/digital.rs
+++ src/digital.rs
@@ -1,5 +1,8 @@
 //! Digital I/O
 
+#[cfg(feature = "unproven")]
+use nb;
+
 /// Single digital output pin
 pub trait OutputPin {
     /// Is the output pin high?
@@ -15,6 +18,27 @@ pub trait OutputPin {
     fn set_high(&mut self);
 }
 
+#[cfg(feature = "unproven")]
+/// TODO
+pub enum Event {
+    /// TODO
+    High,
+    /// TODO
+    Low,
+    /// TODO
+    HighToLow,
+    /// TODO
+    LowToHigh,
+}
+
+#[cfg(feature = "unproven")]
+/// TODO
+pub trait Detector {
+    /// Check if event has happened since last call to `poll`. Returns
+    /// `Ok(())` if it has or `Err(nb::Error::WouldBlock)` if not.
+    fn poll(&self) -> nb::Result<(), !>;
+}
+
 /// Single digital input pin
 #[cfg(feature = "unproven")]
 pub trait InputPin {
@@ -23,4 +47,14 @@ pub trait InputPin {
 
     /// Is the input pin low?
     fn is_low(&self) -> bool;
+}
+
+#[cfg(feature = "unproven")]
+/// TODO
+pub trait DetectingInputPin: InputPin {
+    /// TODO
+    type Detector: Detector;
+
+    /// TODO
+    fn detect(&self, event: Event) -> Self::Detector;
 }
